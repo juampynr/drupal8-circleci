977dcb6e8aa3fe0a0a1554c0ab25b640c0bdca29
Juampy <juampynr@gmail.com>
2018-04-02 23:43:23 +0200

    Move coverage report to Robo

--- .circleci/RoboFile.php
+++ .circleci/RoboFile.php
@@ -36,6 +36,21 @@ class RoboFile extends \Robo\Tasks
     }
 
     /**
+     * Command to generate a coverage report.
+     *
+     * @return \Robo\Result
+     *   The result of the collection of tasks.
+     */
+    public function jobGenerateCoverageReport()
+    {
+        $collection = $this->collectionBuilder();
+        $collection->addTask($this->installDependencies());
+        $collection->addTask($this->installDrupal());
+        $collection->addTaskList($this->runUnitTestsWithCoverage());
+        return $collection->run();
+    }
+
+    /**
      * Installs composer dependencies.
      *
      * @return \Robo\Contract\TaskInterface
@@ -81,6 +96,37 @@ class RoboFile extends \Robo\Tasks
             ->exec('../vendor/bin/phpunit -c core --debug --verbose --log-junit ../artifacts/phpunit/phpunit.xml modules/custom');
         return $tasks;
     }
+
+    /**
+     * Run unit tests and generates a code coverage report.
+     *
+     * @return \Robo\Task\Base\Exec[]
+     *   An array of tasks.
+     */
+    protected function runUnitTestsWithCoverage()
+    {
+        $force = true;
+        $tasks = [];
+        $tasks[] = $this->taskFilesystemStack()
+            ->copy('.circleci/config/phpunit.xml', 'web/core/phpunit.xml', $force)
+            ->mkdir('artifacts/coverage-xml', 777)
+            ->mkdir('artifacts/coverage-html', 777);
+        $tasks[] = $this->taskExecStack()
+            ->dir('web')
+            ->exec('../vendor/bin/phpunit -c core --debug --verbose --coverage-xml ../artifacts/coverage-xml --coverage-html ../artifacts/coverage-html modules/custom');
+        return $tasks;
+    }
+
+    /**
+    cp .circleci/config/phpunit.xml web/core/
+    mkdir -p artifacts/coverage-xml
+    mkdir -p artifacts/coverage-html
+    chmod -R 777 artifacts
+    cd web
+    timeout 60m sudo -E -u www-data ../vendor/bin/phpunit --verbose --debug -c core --coverage-xml ../artifacts/coverage-xml --coverage-html ../artifacts/coverage-html --testsuite nonfunctional modules/custom
+    cd ../
+    tar czf artifacts/coverage.tar.gz -C artifacts coverage-html coverage-xml
+     */
 
     /**
      * Return drush with default arguments.
